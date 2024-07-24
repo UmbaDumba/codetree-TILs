e901a293c9b1d39150bff0cae066c4cc418b0520
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-07-24 22:39:34 +0900

    Title: 양수 직사각형의 최대 크기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240724/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 240724/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
-
-bool is_pos(vector<vector<int>> maps, vector<int> left, vector<int> right){
-    int x1 = left[0];
-    int x2 = right[0];
-    int y1 = left[1];
-    int y2 = right[1];
-    for(int i = x1; i<=x2; i++){
-        for(int j = y1; j<=y2; j++){
-            if(maps[i][j] <= 0){
-                return false;
-            }
-        }
-    }
-    return true;
-}
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n, m;
     cin >> n >> m;
     vector<vector<int>> maps(n, vector<int>(m, 0));
+    vector<vector<int>> down_maps(n, vector<int>(m, 0));
     bool flag = true;
     for(int i = 0; i<n; i++){
         for(int j = 0; j<m; j++){
@@ -39,25 +27,33 @@ int main() {
         return 0;
     }
 
-    int maxsize = 0;
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<m; j++){
-            // {i, j} 시작
-            if(maps[i][j] <= 0){
-                // 시작부터 조건에 안맞음
-                continue;
-            }
-            for(int k = i; k<n; k++){
-                for(int l = j; l<m; l++){
-                    if(is_pos(maps, {i, j}, {k, l})){
-                        maxsize = max(maxsize, (k-i+1) * (l - j + 1));
-                    }
+    // 전처리
+    for(int i = n-1; i>=0; i--){
+        for(int j = m-1; j>=0; j--){
+            if(maps[i][j] > 0){
+                if(i+1 >= n){
+                    down_maps[i][j] = 1;
+                }else{
+                    down_maps[i][j] = 1 + down_maps[i+1][j];
                 }
             }
         }
     }
 
-    cout << maxsize << endl;
+    // 탐색
+    int maxsize = 0;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            int downmin = INT_MAX;
+            for(int k = 0; k<m; k++){
+                downmin = min(downmin, down_maps[i][k]);
+                maxsize = max(maxsize, (k-i+1) * (downmin));
+            }
+        }
+    }
+
+    cout << maxsize<<endl;
+
 
     return 0;
 }
