29a035c4deee6c6f6f3675fdd557ed69b852b474
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-07-25 22:52:15 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240725/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dfs(vector<vector<int>> maps, int x, int y, int *ends){
+    if(x == maps.size() && y == maps[0].size()){
+        // 끝에 도달
+        (*ends)++;
+        return;
+    }
+    vector<int> dx = {1, 0};
+    vector<int> dy = {0, 1};
+    for(int i = 0; i<2; i++){
+        int newx = x + dx[i];
+        int newy = y + dy[i];
+        if(newx >= maps.size() || newy >= maps[0].size()){
+            continue;
+        }
+        if(maps[newx][newy] == 1){
+            maps[newx][newy] = -1; // 이미 지나간곳
+            dfs(maps, newx, newy, ends);
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> maps(n, vector<int>(m,0));
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            cin >> maps[i][j];
+        }
+    }
+
+    int ends = 0;
+    dfs(maps, 0, 0, &ends);
+    cout << ends << endl;
+
+    return 0;
+}
