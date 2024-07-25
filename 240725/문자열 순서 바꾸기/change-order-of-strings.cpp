8a4ac7b692f1f3ec09b1ff85a39cc14d239c672b
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-07-25 16:56:49 +0900

    Title: 문자열 순서 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << s2<<endl;
+    cout << s1 << endl;
+    return 0;
+}
