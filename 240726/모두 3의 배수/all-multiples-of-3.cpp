9f980a5a410df043f1d3c4f26a6b00568f38d03f
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-07-26 23:06:47 +0900

    Title: 모두 3의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240726/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for(int i = 0; i<5; i++){
+        int temp;
+        cin >> temp;
+        if(temp % 3 != 0){
+            cout << "0" << endl;
+        }
+    }
+    cout << "1"<<endl;
+
+    return 0;
+}
