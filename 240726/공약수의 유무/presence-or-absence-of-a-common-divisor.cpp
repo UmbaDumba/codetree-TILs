f4cd336655ef046858728c0a5149aef315ebd70a
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-07-26 23:04:15 +0900

    Title: 공약수의 유무, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240726/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// a > b
+int gcd(int a, int b){
+    int mod = a % b;
+    if(mod == 0){
+        return b;
+    }
+    return (b, mod);
+}
+
+vector<int> cds(int n){
+    vector<int> result = {n};
+    for(int i = 1; i<(n+1)/2; i++){
+        if(n % i == 0){
+            result.push_back(i);
+        }
+    }
+    return result;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int gcdnum = gcd(2880, 1920);
+    vector<int> cdnums = cds(gcdnum);
+
+    int a, b;
+    cin >> a >> b;
+    for(int i = a; i<=b; i++){
+        if(find(cdnums.begin(), cdnums.end(), i) != cdnums.end()){
+            cout << "1" << endl;
+            return 0;
+        }
+    }
+    cout << "0"<< endl;
+
+    return 0;
+}
