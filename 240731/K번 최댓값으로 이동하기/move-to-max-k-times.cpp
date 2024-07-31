770353a1ddfc190f78edd3d6bf699af55b48b89b
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-07-31 17:46:13 +0900

    Title: K번 최댓값으로 이동하기, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
+++ 240731/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
@@ -3,20 +3,53 @@
 #include <queue>
 using namespace std;
 
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+
 vector<int> step(vector<vector<int>> maps, int x, int y){
     int num = maps[x][y];
     int nextx = -1;
     int nexty = -1;
     int maxnum = 0;
-    for(int i = 0; i<maps.size(); i++){
-        for(int j = 0; j<maps.size(); j++){
-            if(maps[i][j] > maxnum && maps[i][j] < num){
-                nextx = i;
-                nexty = j;
-                maxnum = maps[i][j];
+    int n = maps.size();
+    
+    queue<vector<int>> myque;
+    myque.push({x, y});
+    while(!(myque.empty())){
+        vector<int> nowi = myque.front();
+        myque.pop();
+        for(int i = 0; i<4; i++){
+            int newx = nowi[0] + dx[i];
+            int newy = nowi[1] + dy[i];
+            if(newx < 0 || newx >=n || newy < 0 || newy >= n){
+                continue;
+            }
+            if(maps[newx][newy] < num){
+                
+                //cout << "push : "<<newx << " "<<newy<<endl;
+                myque.push({newx, newy});
+                if(maxnum < maps[newx][newy]){
+                    nextx = newx;
+                    nexty = newy;
+                    maxnum = maps[newx][newy];
+                }else if(maxnum == maps[newx][newy]){
+                    if(newx < nextx){
+                        nextx = newx;
+                        nexty = newy;
+                        maxnum = maps[newx][newy];
+                    }else if(newx == nextx){
+                        if(newy < nexty){
+                            nextx = newx;
+                            nexty = newy;
+                            maxnum = maps[newx][newy];
+                        }
+                    }
+                }
+                maps[newx][newy] = num;
             }
         }
     }
+
     return {nextx, nexty};
 }
 
@@ -38,6 +71,7 @@ int main() {
 
     for(int i = 0; i<k; i++){
         vector<int> next = step(maps, prev[0], prev[1]);
+        //cout << "next : "<<next[0]<<" "<<next[1]<<endl;
         if(next[0] == -1){
             break;
         }
