29e5a45c01ea5d22a0d38ac95862b1183703b499
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-10-02 16:46:27 +0900

    Title: 사각형 채우기, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241002/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    vector<int> dp(n+1, 0);
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    if(n >= 2){
+        dp[2] = 2;
+    }else{
+        cout << dp[n];
+        return 0;
+    }
+
+    for(int i = 3; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    
+    cout << dp[n];
+
+    return 0;
+}
