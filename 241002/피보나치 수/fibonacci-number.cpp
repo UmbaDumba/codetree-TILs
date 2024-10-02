01d46db3a49c8240abc2e1d13a063bcd4dd96d3b
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-10-02 16:22:13 +0900

    Title: 피보나치 수, Time: 1039ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241002/피보나치 수/fibonacci-number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> memo;
+
+int fibo(int n){
+    if(memo[n] != -1){
+        // 이미 구한값
+        return memo[n];
+    }
+    if(n == 1 || n == 2){
+        memo[n] = 1;
+        return 1;
+    }
+    return fibo(n-1) + fibo(n-2);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    memo = vector<int>(50, -1);
+
+    int n;
+    cin >> n;
+
+    cout << fibo(n);
+
+    return 0;
+}
