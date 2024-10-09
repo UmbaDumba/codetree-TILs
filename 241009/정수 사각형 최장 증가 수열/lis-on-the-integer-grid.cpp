91bfe59ceed6240144907ef5306569924a341bda
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-10-09 13:51:24 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1086ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 241009/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 241009/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -1,61 +1,68 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
+
+int n;
+vector<vector<int>> nums;
+vector<vector<int>> dp;
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+
+void dfs(int x, int y, int dfslens, int* lens){
+    *(lens) = max(*(lens), dfslens);
+    for(int i = 0; i<4; i++){
+        int newx = x + dx[i];
+        int newy = y + dy[i];
+        if(newx < 0 || newx >= n || newy < 0 || newy >= n){
+            continue;
+        }
+        if(nums[x][y] < nums[newx][newy]){
+            if(dp[newx][newy] != 0){
+                // 최대 길이가 이미 구해진 부분
+                // 여기서부터는 또 탐색할필요가 없다!
+                dfslens += dp[newx][newy];
+                *(lens) = max(*(lens), dfslens);
+                return;
+            }
+            dfs(newx, newy, dfslens+1, lens);
+        }
+    }
+}
 
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
-    cin >> n;
-    vector<vector<int>> nums(n, vector<int>(n, 0));
-    vector<vector<int>> dp(n, vector<int>(n, 1));
 
-    int x, y, max_ele;
-    max_ele = 0;
+    cin >> n;
+    nums = vector<vector<int>>(n, vector<int>(n, 0));
+    dp = vector<vector<int>>(n, vector<int>(n, 0));
+
 
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             cin >> nums[i][j];
-            if(nums[i][j] > max_ele){
-                x = i;
-                y = j;
-                max_ele = nums[i][j];
-            }
         }
     }
 
-    queue<pair<int, int>> que;
-    que.push({x, y});
 
-    vector<int> dx = {1, 0, -1, 0};
-    vector<int> dy = {0, 1, 0, -1};
 
-    while(!que.empty()){
-        pair<int, int> index = que.front();
-        que.pop();
-        for(int i = 0; i<4; i++){
-            int newx = index.first + dx[i];
-            int newy = index.second + dy[i];
-            if(newx >= n || newx < 0 || newy >= n || newy < 0){
-                continue;
-            }
-            if(nums[newx][newy] < nums[index.first][index.second]){
-                // 이동가능
-                dp[newx][newy] = dp[index.first][index.second] + 1;
-                que.push({newx, newy});
-            }
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            int lens = 0;
+            // dfs -> 최대길이를 lens에 담아오기
+            dfs(i, j, 1, &lens);
+            dp[i][j] = lens;
         }
     }
-
 
     int result = 0;
     for(int i = 0; i<n; i++){
-        result = max(*max_element(dp[i].begin(), dp[i].end()), result);
+        result = max(result, *max_element(dp[i].begin(), dp[i].end()));
     }
-    
+
     cout << result;
 
+    
     return 0;
 }
