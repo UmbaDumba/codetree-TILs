a5869c84736ac0ba12f04ba7ac20120cf5533f06
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-10-09 14:15:28 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1022ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 241009/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 241009/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -34,6 +34,9 @@ int dfs(int x, int y){
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
     cin >> n;
     nums = vector<vector<int>>(n, vector<int>(n, 0));
