ecdc7bdfe8117b657b5d73b631f94a62cd450d65
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-10-11 16:31:56 +0900

    Title: 동전 거슬러주기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241011/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    vector<int> coins;
+    for(int i = 0; i<n; i++){
+        int temp;
+        cin >> temp;
+        coins.push_back(temp);
+    }
+
+    sort(coins.begin(), coins.end());
+
+    vector<int> dp(m+1, INT_MAX);
+
+    for(int i = 0; i<n; i++){
+        dp[coins[i]] = 1;
+    }// 딱 맞는 코인 한개만 쓰는게 이득
+
+    for(int i = coins[0]+1; i<= m; i++){
+        for(int j = 1; j<i; j++){
+            if(dp[j] == INT_MAX || dp[i-j] == INT_MAX){
+                continue;
+            }
+            dp[i] = min(dp[i], dp[j] + dp[i-j]);
+        }
+    }
+    cout << dp[m];
+
+    return 0;
+}
