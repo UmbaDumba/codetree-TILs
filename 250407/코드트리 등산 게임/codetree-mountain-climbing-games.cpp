56277d6ffb74468ef49612e139131b4b1bdd8ecf
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2025-04-07 15:38:36 +0900

    Title: 코드트리 등산 게임, Time: 8ms, Memory: 4MB, Status: Wrong Answer - Codetree

--- 250407/코드트리 등산 게임/codetree-mountain-climbing-games.cpp
+++ 250407/코드트리 등산 게임/codetree-mountain-climbing-games.cpp
@@ -3,33 +3,41 @@
 #include <algorithm>
 using namespace std;
 
-int get_max(vector<int> segment_tree, int a, int b)
+
+vector<int> sans;
+vector<int> dp;
+vector<int> segment_tree(1000000, -1);
+
+int get_max(int a, int b)
 {
     if (a == b) return segment_tree[a];
     else if (b - a == 1) return segment_tree[a] < segment_tree[b] ? segment_tree[b] : segment_tree[a];
 
     int mid = (a + b) / 2;
 
-    int leftmax = get_max(segment_tree, a, mid);
-    int rightmax = get_max(segment_tree, mid + 1, b);
+    int leftmax = get_max(a, mid);
+    int rightmax = get_max(mid + 1, b);
 
     return leftmax < rightmax ? rightmax : leftmax;
 }
 
-
-long long plays(vector<int> sans, int m_index) {
-
-    long long result = 0;
-    vector<int> dp(sans.size(), 0);
-    vector<int> segment_tree(1000000, -1);
-    int start = 0;
-    for (int i = 0; i < sans.size(); i++)
+void update_dp(void)
+{
+    for (int i = dp.size(); i < sans.size(); i++)
     {
         //dp[i] = *max_element(segment_tree.begin(), segment_tree.begin() + sans[i]) + 1;
-        dp[i] = get_max(segment_tree, 0, sans[i] - 1) + 1;
+        dp.push_back(get_max(0, sans[i] - 1) + 1);
         segment_tree[sans[i]] = dp[i];
         //cout << "dp["<<i<<"] : "<<dp[i]<<endl;
     }
+}
+
+
+long long plays(int m_index) {
+
+    long long result = 0;
+
+    update_dp();
 
     result = 1000000 * (dp[m_index] + 1 + *max_element(dp.begin(), dp.end())) + sans[max_element(dp.begin(), dp.end()) - dp.begin()];
 
@@ -39,7 +47,6 @@ long long plays(vector<int> sans, int m_index) {
 int main() {
 
     int q;
-    vector<int> sans;
     cin >> q;
 
     for (int i = 0; i < q; i++)
@@ -55,23 +62,27 @@ int main() {
                 cin >> temp;
                 sans.push_back(temp);
             }
+            update_dp();
         }
         else if (ques == 200)
         {
             int h;
             cin >> h;
             sans.push_back(h);
+            update_dp();
         }
         else if (ques == 300)
         {
             sans.pop_back();
+            dp.pop_back();
+            segment_tree[sans[sans.size() - 1]]--;
         }
         else if (ques == 400)
         {
             int m_index;
             cin >> m_index;
             m_index--;
-            cout << plays(sans, m_index) << "\n";
+            cout << plays(m_index) << "\n";
         }
     }
 
