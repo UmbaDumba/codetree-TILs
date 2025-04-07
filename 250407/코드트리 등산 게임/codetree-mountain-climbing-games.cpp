f97de5e6b3e66195828644403a6db0065a1ae429
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2025-04-07 12:30:36 +0900

    Title: 코드트리 등산 게임, Time: 10ms, Memory: 1MB, Status: Wrong Answer - Codetree

--- 250407/코드트리 등산 게임/codetree-mountain-climbing-games.cpp
+++ 250407/코드트리 등산 게임/codetree-mountain-climbing-games.cpp
@@ -3,22 +3,34 @@
 #include <algorithm>
 using namespace std;
 
-long long plays(vector<int> sans, int m_index){
-    
-    long long result = 0;
-    vector<long long> dp(sans.size(), 0);
-    int start = 0;
-    for(int i = 1; i<sans.size(); i++)
+vector<int> sans;
+vector<long long> dp(50000);
+int dp_index = 0;
+
+void calculate_dp(int san_max)
+{
+    if(dp_index < san_max)
     {
-        for(int j = 0; j<i; j++)
+        // dp 뒷부분 더 계산해야함
+        for(int i = dp_index; i<san_max; i++)
         {
-            if(sans[j] < sans[i])
+            for(int j = 0; j<i; j++)
             {
-                dp[i] = (dp[i] < (dp[j] + 1000000)) ? dp[j] + 1000000 : dp[i];
+                if(sans[j] < sans[i])
+                {
+                    dp[i] = (dp[i] < (dp[j] + 1000000)) ? dp[j] + 1000000 : dp[i];
+                }
+                
             }
-            
         }
     }
+}
+
+long long plays(int m_index){
+    
+    long long result = 0;
+
+    calculate_dp(sans.size());
 
     result = dp[m_index] + 1000000 + *max_element(dp.begin(), dp.end()) + sans[max_element(dp.begin(), dp.end()) - dp.begin()];
 
@@ -28,7 +40,7 @@ long long plays(vector<int> sans, int m_index){
 int main() {
     
     int q;
-    vector<int> sans;
+    
     cin >> q;
 
     for(int i = 0; i<q; i++)
@@ -52,12 +64,13 @@ int main() {
         }else if(ques == 300)
         {
             sans.pop_back();
+            dp_index--;
         }else if(ques == 400)
         {
             int m_index;
             cin >> m_index;
             m_index--;
-            cout << plays(sans, m_index) << "\n";
+            cout << plays(m_index) << "\n";
         }
     }
 
