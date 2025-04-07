5b1f34e65bfd6fbcc94e8a42b5ac4c84e0590b80
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2025-04-07 12:58:50 +0900

    Title: 코드트리 등산 게임, Time: 8ms, Memory: 1MB, Status: Wrong Answer - Codetree

--- 250407/코드트리 등산 게임/codetree-mountain-climbing-games.cpp
+++ 250407/코드트리 등산 게임/codetree-mountain-climbing-games.cpp
@@ -3,24 +3,36 @@
 #include <algorithm>
 using namespace std;
 
-long long plays(vector<int> sans, int m_index){
-    
-    long long result = 0;
-    vector<int> dp(sans.size(), 0);
-    int start = 0;
-    for(int i = 1; i<sans.size(); i++)
+vector<int> sans;
+vector<long long> dp(50000);
+int dp_index = 0;
+
+void calculate_dp()
+{
+    // dp 뒷부분 더 계산해야함
+    for(int i = dp_index; i<sans.size(); i++)
     {
+        dp[i] = 0;
         for(int j = 0; j<i; j++)
         {
             if(sans[j] < sans[i])
             {
-                dp[i] = (dp[i] < (dp[j] + 1)) ? dp[j] + 1 : dp[i];
+                dp[i] = (dp[i] < (dp[j] + 1000000)) ? dp[j] + 1000000 : dp[i];
             }
             
         }
     }
+    dp_index = sans.size()-1;
+    
+}
 
-    result = 1000000 * (dp[m_index] + 1 + *max_element(dp.begin(), dp.end())) + sans[max_element(dp.begin(), dp.end()) - dp.begin()];
+long long plays(int m_index){
+    
+    long long result = 0;
+
+    calculate_dp();
+
+    result = dp[m_index] + 1000000 + *max_element(dp.begin(), dp.end()) + sans[max_element(dp.begin(), dp.end()) - dp.begin()];
 
     return result;
 }
@@ -28,7 +40,7 @@ long long plays(vector<int> sans, int m_index){
 int main() {
     
     int q;
-    vector<int> sans;
+    
     cin >> q;
 
     for(int i = 0; i<q; i++)
@@ -44,20 +56,26 @@ int main() {
                 cin >> temp;
                 sans.push_back(temp);
             }
+
+            calculate_dp();
+
         }else if(ques == 200)
         {
             int h;
             cin >> h;
             sans.push_back(h);
+
+            calculate_dp();
         }else if(ques == 300)
         {
             sans.pop_back();
+            dp_index--;
         }else if(ques == 400)
         {
             int m_index;
             cin >> m_index;
             m_index--;
-            cout << plays(sans, m_index) << "\n";
+            cout << plays(m_index) << "\n";
         }
     }
 
